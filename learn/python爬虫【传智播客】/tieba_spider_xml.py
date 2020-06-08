6d34f4be5e4ec1110ed5ebd132c06cef5e72d3bd
zgg <sdut2012@163.com>
2020-06-09 00:27:29 +0800

    update

--- learn/python爬虫【传智播客】/tieba_spider_xml.py
+++ learn/python爬虫【传智播客】/tieba_spider_xml.py
@@ -1,38 +1,40 @@
 from urllib import request,parse
+
+import requests
 from lxml import etree
 
-def writeImage(link,headers):
+def writeImage(link):
     """
     将html内容写入到本地
     :param link:
-    :param headers:
     :return:
     """
-    req = request.Request(link, headers=headers)
-    response = request.urlopen(req)
-    image = response.read().decode()
+    # 6.发送请求，获取图片的服务器响应文件
+    response_image = requests.get(link)
+
     filename = link[-10:]
     # 写入到本地磁盘文件内
     path = "out//%s " % filename
+    # 7.写入文件
     with open(path, "wb") as f:
-        f.write(image)
+        f.write(response_image.content)
     print("已经成功下载 " + path)
 
-def loadLink(link,headers):
+def loadLink(link):
     """
     取出每个帖子里的每个图片连接
     :param link:
-    :param headers:
     :return:
     """
-    req = request.Request(link, headers=headers)
-    response = request.urlopen(req)
-    html = response.read().decode()
+    # 4.发送请求，获取帖子的链接的服务器响应文件
+    response = requests.get(link)
+    response.encoding = 'utf8'
 
-    content = etree.HTML(html)
+    content = etree.HTML(response.text)
+    # 5.分析页面，过滤出图片的链接
     link_list = content.xpath('//img[@class="BDE_Image"]/@src')
     for link in link_list:
-        writeImage(link,headers)
+        writeImage(link)
 
 def loadPage(fullUrl):
     """
@@ -44,19 +46,20 @@ def loadPage(fullUrl):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.44",
     }
-    req = request.Request(fullUrl, headers=headers)
-    response = request.urlopen(req)
-    html = response.read().decode()
-    #print(html)
-    content = etree.HTML(html)
-    # link_list = content.xpath('//div[@class="t_con cleafix"]/div/div/div/a/@href')
-    link_list = content.xpath('//div[@class="t_con cleafix"]/div//a/@href')
+    # 2.发送请求，网页链接的服务器响应文件
+    response = requests.get(fullUrl)
+    response.encoding = 'utf8'
 
+    # 解析html 为 HTML 文档
+    content = etree.HTML(response.text)
+    # 3.分析页面，过滤出参数，拼接获取每个帖子的链接
+    link_list = content.xpath('//div[@class="t_con cleafix"]/div/div/div/a/@href')
+    #link_list = content.xpath('//div[@class ="threadlist_lz clearfix"]/div/a/@href')
     #print(link_list)
     for link in link_list:
         fulllink = "http://tieba.baidu.com" + link
         #print(fulllink)
-        loadLink(fulllink,headers)
+        loadLink(fulllink)
 
 def tiebaSpider(url, startPage, endPage):
     """
@@ -66,8 +69,10 @@ def tiebaSpider(url, startPage, endPage):
         endPage : 结束页
     """
     for page in range(startPage, endPage + 1):
-        # http://tieba.baidu.com/f?xxx&pn=0
+        # 1.分析网页链接，发现规律
+        # http://tieba.baidu.com/f?kw=%E7%BE%8E%E5%A5%B3&pn=0
         fullUrl = url + "&" + "pn=%s" % str((page - 1) * 50)
+        #print(fullUrl)
         loadPage(fullUrl)
         # print html
 
---
+++ learn/python爬虫【传智播客】/tieba_spider_xml_2.py
@@ -0,0 +1,51 @@
+import requests
+from lxml import etree
+
+
+class Myspider():
+    def __init__(self):
+        self.post_bar = input('请输入贴吧名：')
+        self.num = 1
+
+    def postBar(self):
+        '''
+        获取贴吧帖子的url
+        :return:
+        '''
+        base_url = 'https://tieba.baidu.com/'
+        url = base_url + 'f?' + 'kw=' + self.post_bar
+        response = requests.get(url)
+        response.encoding = 'utf8'
+        # 解析html 为 HTML 文档，
+        #print(response.text)
+        html = etree.HTML(response.text)
+        # 抓取当前页面的所有帖子的url的后半部分，也就是帖子编号
+        tieName = html.xpath('//div[@class ="threadlist_lz clearfix"]/div/a/@href')
+        for tieUrl in tieName:
+            url = base_url + tieUrl
+            self.imgUrl(url)
+    def imgUrl(self,url):
+        response = requests.get(url)
+        response.encoding = 'utf8'
+        html = etree.HTML(response.text)
+        tieName = html.xpath('//img[@class="BDE_Image"]/@src')
+
+        for tieUrl in tieName:
+            self.loadImg(tieUrl)
+
+    def loadImg(self,url):
+        print(url)
+        print('正在下载第%s张' % self.num)
+        with open('out/' + str(self.num) + '.png', 'wb') as file:
+
+            # 2. 获取图片里的内容
+            images = requests.get(url)
+
+            # 3. 调用文件对象write() 方法，将page_html的内容写入到文件里
+            file.write(images.content)
+
+        self.num +=1
+
+if __name__ =='__main__':
+    imgSpdier = Myspider()
+    imgSpdier.postBar()
