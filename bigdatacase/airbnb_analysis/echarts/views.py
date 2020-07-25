b61cf3aa4af6420d2f168d3b36261df5a29066fb
zgg <sdut2012@163.com>
2020-07-25 16:17:23 +0800

    delete

--- bigdatacase/airbnb_analysis/echarts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import result
-# Create your views here.
-
-def shares_show(request):
-    shares_result = result.objects.all()
-    return render(request, 'echarts.html', locals())
--- bigdatacase/ipaddr_analysis/echarts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ip
-# Register your models here.
-
-admin.site.register(ip)
