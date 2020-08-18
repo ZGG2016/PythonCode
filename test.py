45398a598eac39150cbc338a516de536b87c155b
zgg <sdut2012@163.com>
2020-08-18 19:53:06 +0800

    update

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
 
 import sys,xlrd
+from operator import add
 
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
-        sys.exit(-1)
-    sc = SparkContext(appName="PythonStreamingNetworkWordCount")
-    ssc = StreamingContext(sc, 1)
 
-    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    counts = lines.flatMap(lambda line: line.split(" "))\
-                  .map(lambda word: (word, 1))\
-                  .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+from time import ctime, sleep
 
-    ssc.start()
-    ssc.awaitTermination()
+import time, functools
+
+from pyhive import hive
+
